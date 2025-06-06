73c48147e492afe2d3de3025a5eae0ec1c5de9fa
AngKalinina <angelina.k.data@gmail.com>
2025-06-06 21:52:14 +0400

    add refuse animation

---
+++ Tests/chrom_test.py
@@ -0,0 +1,18 @@
+﻿from selenium import webdriver
+from selenium.webdriver.chrome import options
+
+capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "100.0",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": True
+    }
+}
+
+options.capabilities,update(selenium_capabilities)
+driver = webdriver.Remote(
+    command_executor=f"https://{selenium_login}:{selenium_pass}@{selenium_url}/wd/hub",
+    options=options)
+
+browser.config.driver = driver
