248869d071c5d85fe2910ab4617129b6cbbbf13d
AngKalinina <angelina.k.data@gmail.com>
2025-07-09 23:41:32 +0400

    вынесла визуал в html движок оставила на python

--- launcher/main.py
+++ launcher/main.py
@@ -1,4 +1,15 @@
 ﻿import argparse
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
+
+from bot.handlers import game  # Telegram команды
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 def run_pygame():
@@ -7,24 +18,25 @@ def run_pygame():
     game.run()
 
 
-def run_bot():
-    from launcher.main import main as run_telegram_bot
-    run_telegram_bot()
+async def run_bot():
+    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(game.router)
+    print("Telegram bot started")
+    await dp.start_polling(bot)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Run Tamagoshani in different modes")
     parser.add_argument(
-        "--mode",
-        choices=["pygame", "bot"],
-        default="pygame",
+        "--mode", choices=["pygame", "bot"], default="pygame",
         help="Choose mode to run: 'pygame' for game UI or 'bot' for Telegram bot"
     )
     args = parser.parse_args()
 
     if args.mode == "bot":
         print("Starting Telegram bot...")
-        run_bot()
+        asyncio.run(run_bot())
     else:
         print("Starting Pygame interface...")
         run_pygame()
